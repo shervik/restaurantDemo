d5d9dae41776d8cf941c6f8c4930934f67f416a8
shervik <victoriascher5@gmail.com>
2019-12-03 21:32:12 +0300

    optz

--- src/Work_1/Main.java
+++ src/Work_1/Main.java
@@ -1,11 +1,6 @@
 package Work_1;
 
-import Work_1.Dishes.Bread;
-import Work_1.Dishes.Meatball;
-import Work_1.Dishes.Tea;
 import Work_1.Ingredients.*;
-
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,32 +12,6 @@ public class Main {
         restaurant.addIngredient(new Lipton());
         restaurant.addIngredient(new Sugar());
 
-        for (Ingredient ingredient : restaurant.ingredientList) {
-            System.out.printf("В ресторане имеется %s в размере %d г.\n", ingredient, ingredient.getCount());
-        }
-
-        while ( (restaurant.ingredientList.get(0).getCount() > 0 && restaurant.ingredientList.get(1).getCount() > 0 && restaurant.ingredientList.get(4).getCount() > 0) ||
-                (restaurant.ingredientList.get(0).getCount() > 0 && restaurant.ingredientList.get(2).getCount() > 0) ||
-                (restaurant.ingredientList.get(3).getCount() > 0 && restaurant.ingredientList.get(4).getCount() > 0)) {
-            restaurant.getDish(new Bread());
-            restaurant.getDish(new Meatball());
-            restaurant.getDish(new Tea());
-        }
-
-        System.out.println("");
-
-        for (Map.Entry en : restaurant.dishes.entrySet()) {
-            System.out.printf("Всего в ресторане приготовлено %d шт. %s.\n", (int) en.getValue(), en.getKey());
-        }
-
-        if (restaurant.dishes.containsKey("Хлеб") && restaurant.dishes.containsKey("Фрикаделька") && restaurant.dishes.containsKey("Чай")) {
-            System.out.println("Ужин готов! Садитесь жрать, пожалуйста. \u00A9");
-        }
-
-        if (restaurant.dishes.isEmpty()) {
-            System.out.println("К сожалению, ресторан ничего не приготовил :(");
-        }
-
-
+        restaurant.toOrder();
     }
 }
--- src/Work_1/Restaurant.java
+++ src/Work_1/Restaurant.java
@@ -1,12 +1,14 @@
 package Work_1;
 
+import Work_1.Dishes.Bread;
 import Work_1.Dishes.Dish;
+import Work_1.Dishes.Meatball;
+import Work_1.Dishes.Tea;
 import Work_1.Ingredients.Ingredient;
 
 import java.util.*;
 
 public class Restaurant {
-
     Map<String, Integer> dishes = new HashMap();
     List<Ingredient> ingredientList = new LinkedList<>();
 
@@ -24,4 +26,40 @@ public class Restaurant {
         }
         return dishes;
     }
+
+    public void allIng() {
+        for (Ingredient ingredient : ingredientList) {
+            System.out.printf("В ресторане имеется %s в размере %d г.\n", ingredient, ingredient.getCount());
+        }
+    }
+
+    public void toOrder() {
+        allIng();
+
+        while ((ingredientList.get(0).getCount() > 0 && ingredientList.get(1).getCount() > 0 && ingredientList.get(4).getCount() > 0) || (ingredientList.get(0).getCount() > 0 && ingredientList.get(2).getCount() > 0) || (ingredientList.get(3).getCount() > 0 && ingredientList.get(4).getCount() > 0)) {
+            getDish(new Bread());
+            getDish(new Meatball());
+            getDish(new Tea());
+        }
+
+        System.out.println();
+
+        call();
+    }
+
+    private void call() {
+        for (Map.Entry en : dishes.entrySet()) {
+            System.out.printf("Всего в ресторане приготовлено %d шт. %s.\n", (int) en.getValue(), en.getKey());
+        }
+
+        if (dishes.containsKey("Хлеб") && dishes.containsKey("Фрикаделька") && dishes.containsKey("Чай")) {
+            System.out.println("Ужин готов! Садитесь жрать, пожалуйста. \u00A9");
+        }
+
+        if (dishes.isEmpty()) {
+            System.out.println("К сожалению, ресторан ничего не приготовил :(");
+        }
+    }
+
+
 }
